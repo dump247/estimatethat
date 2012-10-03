22c75b0d50a46e479165784132f59a1a1552a067
Cory Thomas <cory.thomas@bazaarvoice.com>
2012-10-03 16:37:24 -0500

    Moved room and common card definitions to separate module

--- app.js
+++ app.js
@@ -9,144 +9,12 @@ var socket = require('socket.io');
 var http = require('http');
 var packageInfo = require('./package.json');
 
+var rooms = require('./lib/rooms');
+
 var app = express();
 var server = http.createServer(app);
 
 var serverPort = process.env.VMC_APP_PORT || 3000;
-
-var INFINITY_CARD = {
-    value: Number.MAX_VALUE,
-    label: "\u221E"
-};
-
-var QUESTION_CARD = {
-    value: 'question',
-    label: '?'
-};
-
-var BREAK_CARD = {
-    value: 'break',
-    label: "\u2615"
-};
-
-var ROOM_TYPES = [
-    {
-        id: 'shirt-sizes',
-        title: 'Shirt Sizes',
-        code: 'a',
-        cards: [
-            {
-                value: 1,
-                label: 'XS'
-            }, {
-                value: 2,
-                label: 'S'
-            }, {
-                value: 3,
-                label: 'M'
-            }, {
-                value: 4,
-                label: 'L'
-            }, {
-                value: 5,
-                label: 'XL'
-            },
-            INFINITY_CARD,
-            QUESTION_CARD,
-            BREAK_CARD
-        ]
-    },
-
-    {
-        id: 'sequence',
-        title: 'Sequence',
-        code: 'b',
-        cards: [
-            {
-                value: 0,
-                label: '0'
-            }, {
-                value: 1,
-                label: '1'
-            }, {
-                value: 2,
-                label: '2'
-            }, {
-                value: 3,
-                label: '3'
-            }, {
-                value: 4,
-                label: '4'
-            }, {
-                value: 5,
-                label: '5'
-            }, {
-                value: 6,
-                label: '6'
-            }, {
-                value: 7,
-                label: '7'
-            }, {
-                value: 8,
-                label: '8'
-            }, {
-                value: 9,
-                label: '9'
-            }, {
-                value: 10,
-                label: '10'
-            },
-            INFINITY_CARD,
-            QUESTION_CARD,
-            BREAK_CARD
-        ]
-    },
-
-    {
-        id: 'modified-fibonacci',
-        title: 'Modified Fibonacci',
-        code: 'c',
-        cards: [
-            {
-                value: 0,
-                label: '0'
-            }, {
-                value: 0.5,
-                label: "\u00BD"
-            }, {
-                value: 1,
-                label: '1'
-            }, {
-                value: 2,
-                label: '2'
-            }, {
-                value: 3,
-                label: '3'
-            }, {
-                value: 5,
-                label: '5'
-            }, {
-                value: 8,
-                label: '8'
-            }, {
-                value: 13,
-                label: '13'
-            }, {
-                value: 20,
-                label: '20'
-            }, {
-                value: 40,
-                label: '40'
-            }, {
-                value: 100,
-                label: '100'
-            },
-            INFINITY_CARD,
-            QUESTION_CARD,
-            BREAK_CARD
-        ]
-    }
-];
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
@@ -250,7 +118,7 @@ app.api = {
 app.set('apiUrl', app.api.root);
 
 app.api.get('roomTypes', function (request, response) {
-    response.send(200, ROOM_TYPES);
+    response.send(200, rooms);
 });
 
 app.api.get('room/:room_id', function (request, response) {
@@ -300,7 +168,7 @@ app.api.post('create', function (request, response) {
 server.listen(serverPort);
 
 function getRoomType (id) {
-    return _.find(ROOM_TYPES, function (r) { return r.id === id || r.code === id; });
+    return _.find(rooms, function (r) { return r.id === id || r.code === id; });
 }
 
 io = socket.listen(server);
